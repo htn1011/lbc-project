25d7cc52bebe8bc51436044ce41ccd11d64652f3
dvorakyl <kyle.dvorak@mykenzie.snhu.edu>
2022-06-25 17:41:44 -0500

    for ha

--- Application/src/main/java/com/kenzie/appserver/controller/TaskController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/TaskController.java
@@ -1,7 +1,7 @@
 package com.kenzie.appserver.controller;
 
 import com.kenzie.appserver.controller.model.TaskCreateRequest;
-import com.kenzie.appserver.controller.model.ExampleResponse;
+import com.kenzie.appserver.controller.model.TaskResponse;
 
 import com.kenzie.appserver.service.model.Example;
 import org.springframework.http.ResponseEntity;
